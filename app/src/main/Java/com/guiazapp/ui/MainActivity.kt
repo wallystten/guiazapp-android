0c69531dad68ca3a29d0b54a5a9a284f6ad8c696
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-25 18:42:19 -0300

    Update MainActivity.kt

--- app/src/main/Java/com/guiazapp/ui/MainActivity.kt
+++ app/src/main/Java/com/guiazapp/ui/MainActivity.kt
@@ -2,12 +2,17 @@ package com.guiazapp.ui
 
 import android.app.Activity
 import android.os.Bundle
-import com.guiazapp.R
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val textView = TextView(this)
+        textView.text = "GuiaZapp abriu com sucesso 🚀"
+        textView.textSize = 20f
+
+        setContentView(textView)
     }
 }
