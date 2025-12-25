7a5d076d4923df16dd84fab3bc3cd5d09364834d
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-25 17:01:00 -0300

    Create MainActivity.kt

---
+++ app/src/main/Java/com/guiazapp/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.guiazapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.guiazapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
