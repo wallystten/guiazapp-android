98009c30a6571d607948f22e6d73057c55fd48f5
wallystten <alfredosilvabarbosa29@gmail.com>
2025-12-25 18:33:28 -0300

    Update MainActivity.kt

--- app/src/main/Java/com/guiazapp/ui/MainActivity.kt
+++ app/src/main/Java/com/guiazapp/ui/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.guiazapp.ui
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.guiazapp.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 }
